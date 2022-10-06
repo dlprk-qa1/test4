5ad917ec96734a0e1a34aad556349ab8cf384466
dlprk-qa1 <97515913+dlprk-qa1@users.noreply.github.com>
2022-10-06 17:34:47 +0530

    Create s3_test.tf

---
+++ s3_test.tf
@@ -0,0 +1,11 @@
+resource "aws_s3_bucket" "s3_versioning" {
+  bucket = "examplebuckettftest"
+  bucket_acl    = "public-read-write"
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+module "s3-bucket_example_complete" {
+  source  = "terraform-aws-modules/s3-bucket/aws//examples/complete"
+  version = "3.4.0"
+}
