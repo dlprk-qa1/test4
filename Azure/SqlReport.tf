8228ae671e470a516afb130a8f0f34960ac742af
dlprk-qa1 <97515913+dlprk-qa1@users.noreply.github.com>
2022-10-06 20:40:25 +0530

    Add files via upload

---
+++ Azure/SqlReport.tf
@@ -0,0 +1,47 @@
+resource "azurerm_resource_group" "example_sql_report" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_sql_server" "example_sql_report1" {
+  name                         = "mysqlserver"
+  resource_group_name          = azurerm_resource_group.example.name
+  location                     = azurerm_resource_group.example.location
+  version                      = "12.0"
+  administrator_login          = "4dm1n157r470r"
+  administrator_login_password = "4-v3ry-53cr37-p455w0rd"
+}
+
+resource "azurerm_storage_account" "example_sql_report2" {
+  name                     = "accteststorageaccount"
+  resource_group_name      = azurerm_resource_group.example.name
+  location                 = azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+}
+
+resource "azurerm_storage_container" "example_sql_report3" {
+  name                  = "accteststoragecontainer"
+  storage_account_name  = azurerm_storage_account.example.name
+  container_access_type = "private"
+}
+resource "azurerm_mssql_server_security_alert_policy" "example_sql_report4" {
+  resource_group_name = azurerm_resource_group.example.name
+  server_name         = azurerm_sql_server.example.name
+  state               = "Enabled"
+}
+
+resource "azurerm_mssql_server_vulnerability_assessment" "example_sql_report5" {
+  server_security_alert_policy_id = azurerm_mssql_server_security_alert_policy.example.id
+  storage_container_path          = "${azurerm_storage_account.example.primary_blob_endpoint}${azurerm_storage_container.example.name}/"
+  storage_account_access_key      = azurerm_storage_account.example.primary_access_key
+
+  recurring_scans {
+    enabled                   = true
+    email_subscription_admins = true
+    emails = [
+      "email@example1.com",
+      "email@example2.com"
+    ]
+  }
+}
